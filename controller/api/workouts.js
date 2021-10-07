58d77a65395271a20e8f1c734502bfe17ea7f222
Jacob Goldfine <jacobjgoldfine@gmail.com>
2021-10-07 01:06:18 -0500

    added api routes first run

--- controller/api/workouts.js
+++ controller/api/workouts.js
@@ -1,10 +1,27 @@
 const router = require("express").Router();
-const Workouts = require("../../models");
+const Workout = require("../../models");
 
-Workouts.aggregate([
-  {
-    $addFields: {
-      totalDuration: { $sum: "$exercises.duration" },
+router.get("/", (req, res) => {
+  Workout.find({}).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: "$exercises.duration" },
+      },
     },
-  },
-]);
+  ]);
+});
+
+router.put("/", ({ body }, res) => {
+  Workout.insertMany(body).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+});
+
+router.post("/", ({ body }, res) => {
+  Workout.create(body).then((dbWorkout) => {
+    res.json(dbWorkout);
+  });
+});
